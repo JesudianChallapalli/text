e7a5bed48176883d6edbbedda1a4759e031f154e
JesudianChallapalli <challap@uwindsor.ca>
2019-04-12 19:37:35 -0400

    pizza and burger

--- hungry.py
+++ hungry.py
@@ -1,5 +1,7 @@
-hungry = input("Are you hungry ?")
-if hungry=="Yes":
-    print("Eat samosa")
+hungry = input("Are you hungry?")
+if hungry == "Yes":
+      print("Eat samosa")
+      print("Eat pizza")
+      print("Eat burger")
 else:
     print("Do home work")
